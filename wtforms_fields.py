e9a238d0a37b87d9247407d11ff07a7c56ca4ade
Thelorddero <derosam11@southernct.edu>
2020-11-11 16:38:34 -0500

    worked on login

--- wtforms_fields.py
+++ wtforms_fields.py
@@ -3,6 +3,17 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, EqualTo, ValidationError
 from models import User
 
+def invalid_credentials(form, field):
+
+    username_entered = form.username.data
+    password_entered = field.data
+
+    user_object = User.query.filter_by(username=username.data).first()
+    if user_object in None:
+        raise ValidationError("Username or Password is incorrect")
+    elif password_entered != user_object.password:
+        raise ValidationError("Username or Password is incorrect")
+
 class RegistrationForm(FlaskForm):
     username = StringField('username_label',
                            validators=[InputRequired(message="Username required"),
@@ -20,3 +31,11 @@ class RegistrationForm(FlaskForm):
         user_object = User.query.filter_by(username=username.data).first()
         if user_object:
             raise ValidationError("Username already exists. Create another username ")
+
+
+class LoginForm(FlaskForm)
+    username = StringField('username_label',
+                validators=[InputRequired(message="Username required")])
+    password = PasswordField('password_label',
+                             validators=[InputRequired(message="Password required"), invalid_credentials])
+    submit_button = SubmitField('Login')
